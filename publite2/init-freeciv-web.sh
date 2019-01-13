da945cc404b4109a5e4c35f4300f68c7d746bbb4
Máximo Castañeda <antiswe@yahoo.es>
2019-01-13 14:39:17 +0100

    Hack for uploaded game settings
    publite2 checks for new longturn settings files to start new games. We
    use that to start user-provided multiplayer games, making publite2 see
    the longturn file, file teaching the launcher to treat them like
    multiplayer without the quitidle flag.
    
    Not clean and restart doesn't still work, uploading it just to have it
    somewhere safe.

--- publite2/init-freeciv-web.sh
+++ publite2/init-freeciv-web.sh
@@ -18,14 +18,30 @@ if [[ $6 == *"longturn"* ]]; then
   quitidle=" "
 fi
 
-export FREECIV_SAVE_PATH=${1};
+if [ ${6::12} = "longturn_up_" ]; then
+  game_type=multiplayer
+  upname=${6:12}
+  tmpmark=${upname::8}
+  upuser=${upname:9}
+  gamedir="${1}/up/${upuser}/${tmpmark}"
+  pbemcmd="--Ranklog ${gamedir}/rank.log "
+  lastgame=$(ls -t "${gamedir}"/*.sav* 2>/dev/null | head -n 1)
+  if [ -n "${lastgame}" ]; then
+    pbemcmd="${pbemcmd} --file ${lastgame##*/}"
+  fi
+else
+  game_type=$5
+  gamedir=$1
+fi
+
+export FREECIV_SAVE_PATH=${gamedir};
 rm -f /var/lib/tomcat8/webapps/data/scorelogs/score-${2}.log; 
 
 python3 ../freeciv-proxy/freeciv-proxy.py ${3} > ../logs/freeciv-proxy-${3}.log 2>&1 &
 proxy_pid=$! && 
 ${HOME}/freeciv/bin/freeciv-web --debug=1 --port ${2} --keep ${quitidle} --Announce none -e  -m \
--M http://${4} --type ${5} --read pubscript_${6}.serv --log ../logs/freeciv-web-log-${2}.log \
---saves ${1} ${pbemcmd:- } > /dev/null 2> ../logs/freeciv-web-stderr-${2}.log;
+-M http://${4} --type ${game_type} --read pubscript_${6}.serv --log ../logs/freeciv-web-log-${2}.log \
+--saves ${gamedir} ${pbemcmd:- } > /dev/null 2> ../logs/freeciv-web-stderr-${2}.log;
 
 rc=$?; 
 kill -9 $proxy_pid; 
